3b9e16a240ec71318180f0fc64e8e5297f02c417
jose zuluaga <josez3151@gmail.com>
2024-06-30 16:45:45 -0500

    hasta esta parte funciones basicas y logre conectar el proyecto a firebase,el siguiente paso seria poder autenticar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'register_screen.dart'; // Asegúrate de importar la pantalla de registro
 import 'alarm_screen.dart'; // Importa la pantalla de alarma
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
